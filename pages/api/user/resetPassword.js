d130d74f2cb88ab135fe6aa22427f76871eb9b17
Silviu Bocsa <silviubocsa@gmail.com>
2022-02-17 19:22:37 +0200

    update circleci

--- pages/api/user/resetPassword.js
+++ pages/api/user/resetPassword.js
@@ -1,6 +1,7 @@
 import connectDB from "../../../utils/connectDB";
 import Users from "../../../models/userModel";
 import auth from "../../../middleware/auth";
+import bcrypt from 'bcryptjs'
 
 
 connectDB()
@@ -17,7 +18,7 @@ const resetPassword = async (req, res) => {
 	try {
 		const result = await auth(req, res)
 		const { password } = req.body
-		const passwordHash = await bcrypt.hash(password, 12)
+		const passwordHash = await bcrypt.hashSync(password, 12)
 
 		await Users.findOneAndUpdate({ _id: result.id }, { password: passwordHash })
 
