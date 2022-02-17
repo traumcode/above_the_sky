8906038f078ad73968527bf9a997f06a474e435a
Silviu Bocsa <silviubocsa@gmail.com>
2022-02-17 19:11:26 +0200

    update circleci

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
 			<Html lang='en'>
 				<Head>
 					<meta name="description" content="Above The Sky unlimited"/>
+					<meta name="apple-mobile-app-capable" content="yes"/>
 					<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"/>
 					<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"/>
 					<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
